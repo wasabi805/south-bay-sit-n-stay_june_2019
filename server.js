d77d3fde21335db470551b2a078bf8cccdd48c24
‘Tim <timothy.j.ocampo@gmail.com>
2019-06-12 13:30:32 -0700

    edited port in server for heroku deployment.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
